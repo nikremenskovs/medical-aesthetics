2005d2f953a69312e343551b9712dd9da24909bf
Nik <nikremenskovs@gmail.com>
2023-04-22 16:46:50 +0100

    -Add back to top button

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
 import { RouterView } from 'vue-router'
 import TheNavbar from '@/components/TheNavbar.vue'
 import TheFooter from '@/components/TheFooter.vue'
+import TheScrollToTopButton from '@/components/TheScrollToTopButton.vue'
 import GetInTouchButton from '@/components/getInTouch/GetInTouchButton.vue'
 import { ref, onMounted, onUnmounted } from 'vue'
 import { debounce } from 'lodash'
 
 const showNavbar = ref(true)
 const showGetInTouchButton = ref(false)
+const showScrollToTopButton = ref(false)
 const lastScrollPosition = ref(0)
+
+const toTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" })
+}
 
 const updateScroll = debounce(() => {
   const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
@@ -22,9 +28,20 @@ const updateScroll = debounce(() => {
     case currentScrollPosition < lastScrollPosition.value - 100:
       showNavbar.value = true;
       showGetInTouchButton.value = false
+      break;
   }
 
   lastScrollPosition.value = currentScrollPosition
+
+
+  switch (true) {
+    case currentScrollPosition < 500:
+      showScrollToTopButton.value = false
+      break;
+    case currentScrollPosition > 500:
+      showScrollToTopButton.value = true
+      break;
+  }
 }, 180)
 
 
@@ -50,6 +67,11 @@ onUnmounted(() => {
     </transition>
     <RouterView />
     <TheFooter />
+    <transition enter-active-class="transition ease-out duration-500 transform"
+      leave-active-class="transition ease-out duration-1000 transform" enter-from-class="opacity-0"
+      leave-to-class="opacity-0" enter-to-class="translate-x-0">
+      <TheScrollToTopButton v-show="showScrollToTopButton" @click="toTop" />
+    </transition>
   </div>
 </template>
 
