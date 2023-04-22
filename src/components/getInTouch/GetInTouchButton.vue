8a7104e9a0508d6be67afceb918b403e6ef7d722
Nik <nikremenskovs@gmail.com>
2023-04-22 16:58:36 +0100

    -Adjust get in touch button size for responsive design purposes

--- src/components/getInTouch/GetInTouchButton.vue
+++ src/components/getInTouch/GetInTouchButton.vue
@@ -1,6 +1,6 @@
 <template>
     <router-link :to="{ name: 'home' }"
-        class="fixed z-20 top-1/2 right-0 origin-bottom-right -rotate-90 bg-main-blue/75 text-main-white uppercase px-4 py-1 rounded-tl rounded-tr hover:animate-hoverPulse animate-pulse">
+        class="fixed z-20 top-1/2 right-0 origin-bottom-right -rotate-90 bg-main-blue/75 text-main-white uppercase px-4 py-1 rounded-tl rounded-tr hover:animate-hoverPulse animate-pulse md:text-xl lg:text-2xl xl:text-3xl">
         Sazinaties!
     </router-link>
 </template>
