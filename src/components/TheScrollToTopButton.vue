ae8d30aa7b763692f69e1ae86de97d3286d84d7e
Nik <nikremenskovs@gmail.com>
2023-04-23 09:44:40 +0100

    -change arrow to chevron on back to top button

--- src/components/TheScrollToTopButton.vue
+++ src/components/TheScrollToTopButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
         class="fixed z-20 bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 bg-main-blue/50 text-main-white text-xl h-16 w-24 rounded-full hover:animate-hoverPulse ">
-        <i class="fa-solid fa-arrow-up absolute left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/4" />
+        <i class="fa-solid fa-chevron-up absolute left-1/2 top-1/4 -translate-x-1/2 -translate-y-1/4" />
     </button>
 </template>
