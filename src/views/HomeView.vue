358dec14aec088a4fec0a468ee1988a617933475
Nik <nikremenskovs@gmail.com>
2023-05-02 22:12:33 +0100

    Add HomePageAbout section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HomepageBannerSection from "@/components/homepage/HomepageBannerSection.vue";
 import HomepageCardsSection from "@/components/homepage/HomepageCardsSection.vue";
+import HomepageAboutSection from "@/components/homepage/HomepageAboutSection.vue";
 </script>
 
 <template>
   <HomepageBannerSection />
   <HomepageCardsSection />
+  <HomepageAboutSection />
 </template>
