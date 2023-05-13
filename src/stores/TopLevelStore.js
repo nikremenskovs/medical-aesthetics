ed60f43ba9521dadbeb7c7cffa23ad121f37eb42
Nik <nikremenskovs@gmail.com>
2023-05-13 09:59:00 +0100

    remove commented code

--- src/stores/TopLevelStore.js
+++ src/stores/TopLevelStore.js
@@ -3,7 +3,6 @@ import { getTopLevel } from "@/services/squidexService.js";
 
 export const useTopLevelStore = defineStore("TopLevelStore", {
   state: () => ({
-    // selectedLanguage: localStorage.getItem('selectedLanguage') ? localStorage.getItem('selectedLanguage') : 'lv',
     selectedLanguage: localStorage.getItem('selectedLanguage') ?? 'lv',
     favicon: "",
     floatingButton: {},
