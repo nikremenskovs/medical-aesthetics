1e994a05692a386849cc999019f758f70b5285f9
Nik <nikremenskovs@gmail.com>
2023-05-13 08:59:40 +0100

    Add animations + hover to homepage about section

--- src/components/shared/ResponsiveImage.vue
+++ src/components/shared/ResponsiveImage.vue
@@ -2,16 +2,17 @@
     <picture>
         <source v-for="(source, index) in props.sourcesMap" :key="index"
             :srcset="`${props.imgSrc}?width=${source.assetWidth}`" :media="`(min-width: ${source.media})`" />
-        <img class="responsive-position w-full h-full object-cover block object-center" :class="props.cardsSectionTriggerIndex !== null
-            ? `homepageCardImage${props.cardsSectionTriggerIndex}`
-            : ''
-        " :src="props.imgSrc" :alt="props.imgAlt" ref="sectionTwoImage" />
+        <img class="responsive-position w-full h-full object-cover block object-center"
+            :class="props.cardsSectionTriggerIndex !== null
+                ? `homepageCardImage${props.cardsSectionTriggerIndex}` : props.aboutSectionForegroundClass ? 'absolute top-0 md:hover:scale-105 transition duration-[2000ms]' : ''"
+            :src="props.imgSrc" :alt="props.imgAlt" ref="sectionTwoImage" />
     </picture>
 </template>
   
 <script setup>
 const props = defineProps({
     cardsSectionTriggerIndex: { Number, required: false, default: null },
+    aboutSectionForegroundClass: { String, required: false, default: '' },
     imgSrc: { type: String, required: true },
     imgAlt: {
         type: String,
